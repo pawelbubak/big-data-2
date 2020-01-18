471e9a9ea090b7cc921f77b5b63e415ad125e05c
Paweł Bubak <pawelbubak97@gmail.com>
2020-01-18 13:01:21 +0100

    Edit FillTables script

--- FillTables.sh
+++ FillTables.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-spark-submit --class Time --master yarn --num-executors 5 --driver-memory 512m \
-    --executor-memory 512m --executor-cores 1 time.jar
-
-spark-submit --class Location --master yarn --num-executors 5 --driver-memory 512m \
+spark-submit --class LocationETL --master yarn --num-executors 5 --driver-memory 512m \
     --executor-memory 512m --executor-cores 1 location.jar
 
-spark-submit --class LocationScore --master yarn --num-executors 5 --driver-memory 512m \
+spark-submit --class LocationScoreETL --master yarn --num-executors 5 --driver-memory 512m \
     --executor-memory 512m --executor-cores 1 location-score.jar
 
-spark-submit --class Price --master yarn --num-executors 5 --driver-memory 512m \
+spark-submit --class PriceETL --master yarn --num-executors 5 --driver-memory 512m \
     --executor-memory 512m --executor-cores 1 price.jar
+
+spark-submit --class TimeETL --master yarn --num-executors 5 --driver-memory 512m \
+    --executor-memory 512m --executor-cores 1 time.jar
+
+spark-submit --class FactETL --master yarn --num-executors 5 --driver-memory 512m \
+    --executor-memory 512m --executor-cores 1 fact.jar
